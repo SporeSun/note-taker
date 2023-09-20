8c012781e334c0bb3a82bc78ce6a6de3c960256d
SporeSun <135516150+SporeSun@users.noreply.github.com>
2023-09-20 14:54:24 -0700

    completed server.js

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-const dbFilePath = path.join(__dirname, './db.json');
+const PORT = process.env.PORT || 3001;
+const dbFilePath = path.join(__dirname, './db/db.json');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
 // HTML Routes
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 // API Routes
 app.get('/api/notes', (req, res) => {
